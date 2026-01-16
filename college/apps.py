1625e0f209c1e1b419a5913358c48f7840352460
abid450 <sayemhsan25784@gmail.com>
2026-01-15 21:32:52 -0800

    Implemented JWT Authentication module

--- college/apps.py
+++
@@ -1,9 +0,0 @@
-
-from django.apps import AppConfig
-
-class CollegeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'college'
-
-    def ready(self):
-        import college.signals
